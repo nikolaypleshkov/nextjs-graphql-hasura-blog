a7b467052151331cdc8f79b658f57b415917cafb
nikolayevolution <stu2001321014@uni-plovdiv.bg>
2022-06-22 14:50:51 +0300

    refactor: cleaner page components

---
+++ pages/login.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+import React from "react";
+import { LoginPage } from "../src/components/pages";
+
+const Login: NextPage = () => <LoginPage />
+
+export default Login;
---
+++ pages/register.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from 'next'
+import React from 'react'
+import { RegisterPage } from '../src/components/pages'
+
+const Register: NextPage = () => <RegisterPage />
+
+
+export default Register
